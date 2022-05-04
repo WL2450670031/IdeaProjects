a1a266b2d5bf47c0aee74e9ff27d03b151d42bc2
wl2450670031 <wl1030ds@163.com>
2022-05-04 18:12:03 +0800

    ok

--- recursion/src/R21/Main.java
+++ recursion/src/R21/Main.java
@@ -5,24 +5,30 @@ import LinkedList.Linked_Node;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main
+{
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
         int length;
+
         Linked_List l1 = new Linked_List();
         Linked_List l2 = new Linked_List();
-        Linked_List l = new Linked_List();
+        Linked_List l = new Linked_List();//存储结果的链表
         Linked_Node head = new Linked_Node();
+
         System.out.println("l1长度:");
         l1.set_Linked_List(in.nextInt());
         System.out.println("l2长度:");
-        l2.set_Linked_List(in.nextInt());
+        l2.set_Linked_List(in.nextInt());//输入两个链表的数据
 
         length = l1.return_length()+l2.return_length()-1;
+
         l1.search();
-        l2.search();
+        l2.search();//将两个链表进行排序
+
         head.setNext(recursion_search(l1.return_Head_Node().getNext(),l2.return_Head_Node().getNext()));//传头数据
+
         l.setHead(head);
         l.setLength(length);
         l.print_All();
