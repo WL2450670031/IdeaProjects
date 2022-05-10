edd772ee905f1e8464a8ea3f5e9d1c76ffc752f0
wl2450670031 <wl1030ds@163.com>
2022-05-10 12:50:52 +0800

    测时间

--- hello/src/hello.java
+++ hello/src/hello.java
@@ -2,8 +2,17 @@ public class hello
 {
     public static void main(String[] args)
     {
+        long begin = System.currentTimeMillis();
+
         System.out.println("hello world!");
         System.out.println("hello java!");
         System.out.println("我是吴乐。");
+        for(int i = 0;i < 10000000;i++)
+        {
+            System.out.println(i);
+        }
+        long end = System.currentTimeMillis();
+        long time = end - begin ;
+        System.out.println(time +"ms");
     }
 }
