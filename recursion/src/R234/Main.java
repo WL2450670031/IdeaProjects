28a0382042f1110cc7b82c170fb6c5348dc21aec
wl2450670031 <wl1030ds@163.com>
2022-05-23 21:30:02 +0800

    序列化，反序列化

---
+++ recursion/src/R234/Main.java
@@ -0,0 +1,9 @@
+package R234;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ recursion/src/R234/isHuiwen.java
@@ -0,0 +1,9 @@
+package R234;
+
+public class isHuiwen
+{
+    public boolean TheNumberIsHuiwen()
+    {
+
+    }
+}
