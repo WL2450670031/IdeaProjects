7afa11979170711ab8a430d8efbfd6bc96bd15aa
吴乐 <wl1030ds@163.com>
2022-06-01 19:38:38 +0800

    ok

--- Intetnet/src/Test1/Main.java
+++ Intetnet/src/Test1/Main.java
@@ -12,7 +12,7 @@ public class Main
 {
     public static void main(String[] args) throws IOException
     {
-        try(ServerSocket serverSocket = new ServerSocket(8189))
+        try(ServerSocket serverSocket = new ServerSocket(8080))
                 //监听端口服务器套接字
         {
             try(Socket socket = serverSocket.accept())
