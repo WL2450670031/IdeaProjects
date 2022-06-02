810486fb2b9b5d8641e55e7779f2c364c0fd7988
吴乐 <wl1030ds@163.com>
2022-06-03 00:16:06 +0800

    网络编程初学,文件上传。

--- JavaInternet/src/FileUpdate/ClientTest.java
+++ JavaInternet/src/FileUpdate/ClientTest.java
@@ -3,7 +3,6 @@ package FileUpdate;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
-
 public class ClientTest
 {
     public static void main(String[] args) throws IOException
@@ -25,23 +24,28 @@ public class ClientTest
         //4.输出文件
         byte[] buf = new byte[1024];
         int length;
+
         while((length = fileInputStream.read(buf)) != -1)
         {
             outputStream.write(buf,0,length);
         }
 
-        //5.确定服务器接受完毕才端口连接。
-        socket.shutdownOutput();//通知服务器上传完毕
+        //5.确定服务器接受完毕才断开连接。
+        socket.shutdownOutput();
+        //通知服务器上传完毕
+
         InputStream inputStream = socket.getInputStream();
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         byte[] buf2 = new byte[1024];
         int length2;
+
         while((length2 = inputStream.read(buf2)) != -1)
         {
             byteArrayOutputStream.write(buf2,0,length2);
         }
         System.out.println(byteArrayOutputStream);
-        //5.关闭资源,先建后关
+
+        //6.关闭资源,先建后关
         byteArrayOutputStream.close();
         fileInputStream.close();
         outputStream.close();
--- JavaInternet/src/FileUpdate/ServerTest.java
+++ JavaInternet/src/FileUpdate/ServerTest.java
@@ -5,24 +5,32 @@ import java.net.ServerSocket;
 import java.net.Socket;
 public class ServerTest
 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException
+    {
         //1.指定一个端口，提供服务。
         int port = 9000;
         ServerSocket serverSocket = new ServerSocket(port);
+
         //2.监听客户端连接
-        Socket socket = serverSocket.accept();//阻塞式监听，会一直等待客户端连接,连接后才会继续执行代码。
+        Socket socket = serverSocket.accept();
+        //阻塞式监听，会一直等待客户端连接,连接后才会继续执行代码。
+
         //3.获取客户端输入流
         InputStream inputStream = socket.getInputStream();
-        //4.输出文件
+
+        //4.保存文件
         String filePath = "D:\\搞怪.mp4";
         File file = new File(filePath);
         FileOutputStream fileOutputStream = new FileOutputStream(file);
+
         byte[] buf = new byte[1024];
         int length;
         int a = 0;
+
         while((length = inputStream.read(buf))!=-1)
         {
             fileOutputStream.write(buf,0,length);
+            //将上传的文件保存到本地
             a += length;
             System.out.println("已经上传了" + a +"byte");
         }
