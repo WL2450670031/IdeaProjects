f7c08ca598685f972990c7449b6598ecdd36a034
吴乐 <wl1030ds@163.com>
2022-06-03 00:25:37 +0800

    ok

--- Intetnet/src/Test4/Main4.java
+++ Intetnet/src/Test4/Main4.java
@@ -9,30 +9,27 @@ public class Main4
         {
             long a = System.currentTimeMillis();
             // 初始化DownUtil对象
-            final Download downUtil=new Download("https://download.jetbrains.com.cn/"
-                    + "idea/ideaIC-2022.1.1.exe?_gl=1*ttf7rf*_ga*MTg1MjQ0NzE5OC4xNjQwNTkzNjI0*_ga_9J976DJZ68*MTY1MjQ1MTE4My4yLjEuMTY1MjQ1MTMwNi41OQ.."
-                    , "ideaIC-2022.1.1.exe", 4);
+            //
+            final Download downUtil=new Download("https://dlcdn.apache.org/"
+                    + "tomcat/tomcat-9/v9.0.63/bin"
+                    , "apache-tomcat-9.0.63.zip", 4);
             // 开始下载
             downUtil.download();
 
-            new Thread()
+            new Thread(() ->
             {
-                @Override
-                public void run()
+                while(downUtil.getCompleteRate() < 1)
                 {
-                    while(downUtil.getCompleteRate() < 1)
+                    // 每隔0.1秒查询一次任务的完成进度
+                    // GUI程序中可根据该进度来绘制进度条
+                    System.out.println("已完成："+ downUtil.getCompleteRate());
+                    try
                     {
-                        // 每隔0.1秒查询一次任务的完成进度
-                        // GUI程序中可根据该进度来绘制进度条
-                        System.out.println("已完成："+ downUtil.getCompleteRate());
-                        try
-                        {
-                            Thread.sleep(1000);
-                        }
-                        catch (Exception ex){}
+                        Thread.sleep(1000);
                     }
+                    catch (Exception ex){}
                 }
-            }.start();
+            }).start();
             long b = System.currentTimeMillis();
             System.out.println((b-a)/1000+"s");
         }
